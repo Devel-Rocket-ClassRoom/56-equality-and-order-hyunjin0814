1cbdc2ac7e011ebaded4fd793b2055447dca3156
hyunjin0814 <zse846@gmail.com>
2026-03-17 15:41:31 +0900

    56장 중간저장

--- AuctionHouse/Program.cs
+++ AuctionHouse/Program.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine();
+List<AuctionItem> items = new List<AuctionItem>
+{
+    new AuctionItem("전설의 검", 50000, 12, "무기"),
+};
+Console.WriteLine("=== 입찰가 기준 정렬 (BidComparer) ===");
